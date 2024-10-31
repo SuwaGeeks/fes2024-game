17d22ab125cfc3998b049b7e42df02f9acd35567
Yudai Fujimoto <apricot4096@gmail.com>
2024-10-31 23:24:29 +0900

    feat: interface of UI class

---
+++ src/ui/ui.py
@@ -0,0 +1,12 @@
+import pygame as pg
+
+class UI(pg.sprite.Sprite):
+    
+    def __init__(self):
+        pass
+    
+    def update(self):
+        pass
+    
+    def blit(self, surface):
+        pass
